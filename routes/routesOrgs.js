0e3dbd16cdc75f2d3890c04f0be6bfb30ad5eb7c
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-05 19:13:38 +0200

    Añadido get block genesis

--- routes/routesOrgs.js
+++ routes/routesOrgs.js
@@ -6,6 +6,11 @@ module.exports = function (app, redFabric) {
     app.get("/getpeers", function (req, res) {
         var peers = redFabric.getPeers();
         console.log(peers);
+        var temp = [];
+        for (let i = 0; i < peers.length; i++) {
+            const peer = peers[i];
+            
+        }
         res.send({
             peers: peers
         });
