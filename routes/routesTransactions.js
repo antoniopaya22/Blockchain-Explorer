e753e527dc91ed6d98f72cff6596d3726291a05b
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-05 12:15:49 +0200

    Añadido get transaction

--- routes/routesTransactions.js
+++ routes/routesTransactions.js
@@ -4,7 +4,7 @@ module.exports = function (app, redFabric) {
      * GET Transaction
      */
     app.get("/getTransaction/:id", function (req, res) {
-        console.log(red.params.id);
+        console.log(req.params.id);
         redFabric.getTranstaction(parseInt(req.params.id)).then(function (transaction) {
             res.send(transaction);
         }).catch(err => {
