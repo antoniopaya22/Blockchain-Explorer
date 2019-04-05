f2646cb0f8147822c30163c93e6f1c5a87587111
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-05 11:53:16 +0200

    Añadido get num blocks

--- routes/routesBlocks.js
+++ routes/routesBlocks.js
@@ -19,6 +19,12 @@ module.exports = function (app, redFabric) {
      */
     app.get("/lastblock", function (req, res) {
         redFabric.getLastBlock().then(function (block) {
+            var block = {
+                "number":block.header.number,
+                "prevHash":block.header.previous_hash,
+                "dataHash":block.header.data_hash,
+                "channel":block.data.data[0].payload.header.channel_id
+            }
             res.send(block);
         }).catch(err => {
             res.status(500).send("Vaya por dios: " + err);
