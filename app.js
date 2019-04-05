eef2be342518521cc45a0a00c31851732e053674
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-05 11:31:21 +0200

    Añadido get num blocks

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ const RedFabric = require("./modules/redFabric");
 
 
 //==========VARIABLES===============
-var indexRouter = require('./routes/index');
 app.set('port', 8090);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -41,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 //==========RUTAS================
-require("./routes/routesDatos.js")(app, redFabric);
+require("./routes/routesBlocks.js")(app, redFabric);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
