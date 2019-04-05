9312b7b2e64262ab52954c65e98e59ef00025fc3
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-05 11:56:09 +0200

    Añadido get num blocks

--- routes/routesBlocks.js
+++ routes/routesBlocks.js
@@ -23,7 +23,7 @@ module.exports = function (app, redFabric) {
                 "number":block.header.number,
                 "prevHash":block.header.previous_hash,
                 "dataHash":block.header.data_hash,
-                "channel":block.data.data[0].payload
+                "channel":block.data.data[0].payload.header.channel_header.channel_id
             }
             res.send(block);
         }).catch(err => {
