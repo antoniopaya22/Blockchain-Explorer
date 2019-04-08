be703a4486f55b8e1cee11f5475b23011d34cf72
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-08 10:51:33 +0200

    Añadidas vistas

--- routes/routesViews.js
+++ routes/routesViews.js
@@ -3,13 +3,12 @@ module.exports = function (app, redFabric, swig) {
     app.get("/", function (req, res) {
         redFabric.getNumBlocks().then(function (numBlocks) {
             redFabric.getLastBlock().then(function (lastBlock) {
-                console.log(numBlocks);
-                swig.renderFile('views/home.html', {
+                res.send(swig.renderFile('views/home.html', {
                     title: 'ArcelorMittal Blockchain Dashboard',
                     numBlocks: numBlocks,
                     peers: redFabric.getPeers(),
                     lastBlock: lastBlock
-                });
+                }));
             }).catch(err => {
                 res.status(500).send("Vaya por dios: " + err);
             });
