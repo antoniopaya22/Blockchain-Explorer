fbbf75a3bf55c6b05aed7081c9a56d4a333b96f0
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-11 09:21:59 +0200

    Cambiado grafica dias

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ var swig = require('swig');
 //==========VARIABLES===============
 app.set('port', 8090);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 let redFabric = new RedFabric("user1");
 
 //==========INICIACION=============
@@ -56,10 +55,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  var message = err.message;
+  var error = req.app.get('env') === 'development' ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  var respuesta = swig.renderFile('views/error.html', {
+    message: message,
+    error: error,
+  });
+  res.send(respuesta);
 });
 
 
