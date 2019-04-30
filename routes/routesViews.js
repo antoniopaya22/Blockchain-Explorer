4ff4f3511d39f65fd7bb5f03867a7ce88bcff709
antonioalfa22 <antonioalfa22@gmail.com>
2019-04-30 09:43:50 +0200

    Añadida paginacion

--- routes/routesViews.js
+++ routes/routesViews.js
@@ -43,7 +43,10 @@ module.exports = function (app, redFabric, swig) {
     app.get("/bloques", function (req, res) {
         redFabric.getNumBlocks().then(function (numBlocks) {
             var pages = numBlocks%5 == 0 ? parseInt(numBlocks/5) : ((parseInt(numBlocks/5))+1);
-            redFabric.getAllBlocks().then(function (blocks) {
+            var p = req.query.page;
+            if (p === undefined) p = 1;
+            var page = numBlocks - ((p-1)*5);
+            redFabric.getBlocksBt(page).then(function (blocks) {
                 res.send(swig.renderFile('views/bloques.html', {
                     numBlocks: numBlocks.toString(),
                     peers: redFabric.getPeers(),
