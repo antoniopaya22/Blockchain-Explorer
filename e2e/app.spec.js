3a95dffdf68800b7d621e1457beb4a69cc4aa91c
antonioalfa22 <antonioalfa22@gmail.com>
2019-05-30 12:03:10 +0200

    Añadido texto al boton

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -22,6 +22,16 @@ describe('Caso de uso 6', function() {
       browser.waitForAngularEnabled(false);
       browser.get('http://156.35.163.141:8090/buscar');
   
+      element(by.id('txid')).sendKeys('e9c862446dbab024f0');
+      element(by.id('buscarBt')).click();
+
+      expect(element(by.css('.error')).getText()).toBe('500');
+    });
+
+    it('Ver informacion general de la blockchain', function() {
+      browser.waitForAngularEnabled(false);
+      browser.get('http://156.35.163.141:8090/buscar');
+  
       element(by.id('txid')).sendKeys('e9c862446dbab024f04a8806fce0d40fb0e5a73090cdfde0651a623b51cc2e98');
       element(by.id('buscarBt')).click();
 
